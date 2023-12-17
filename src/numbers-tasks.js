6da20e5e63b9ccc91f904860abcba0165b3c7c4b
Dina P <dinchik@bk.ru>
2023-12-17 04:39:57 +0400

    feat: add solution for the twenty eighth task numbers-tasks block

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -507,8 +507,9 @@ function getIntegerOnString(str, base) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  const resultIsSafeInteger = Number.isSafeInteger(number);
+  return resultIsSafeInteger;
 }
 
 /**
